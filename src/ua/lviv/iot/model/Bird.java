af325abf6d0b05bc99bf54279a9fae0f33f57cc6
Yulia Shvets <yuliia-mariia.shvets.ir.2021@lpnu.ua>
2022-05-04 14:00:49 +0300

    Added 2 lab

---
+++ src/ua/lviv/iot/model/Bird.java
@@ -0,0 +1,50 @@
+package ua.lviv.iot.model;
+
+public class Bird {
+    private String name;
+    private String colour;
+    private double weight; // kg
+    private double speed;  // km/h
+    private double feed;         // kg/day
+    private int lifeTime;// years
+    private boolean isBirdFlying;
+
+
+    public boolean isBirdFlying() {
+        return isBirdFlying;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getFeed() {
+        return feed;
+    }
+
+    public Bird(String name, String colour, double weight, double speed, double feed, int lifeTime, boolean isBirdFlying) {
+        this.name = name;
+        this.colour = colour;
+        this.weight = weight;
+        this.speed = speed;
+        this.feed = feed;
+        this.lifeTime = lifeTime;
+        this.isBirdFlying = isBirdFlying;
+    }
+
+    public Bird() {
+    }
+
+    @Override
+    public String toString() {
+        return "Bird{" +
+                "name='" + name + '\'' +
+                ", colour='" + colour + '\'' +
+                ", weight=" + weight +
+                ", speed=" + speed +
+                ", feed=" + feed +
+                ", lifeTime=" + lifeTime +
+                ", isBirdFlying=" + isBirdFlying +
+                '}';
+    }
+}
