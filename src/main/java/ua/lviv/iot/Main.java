0f7e45b59737c53e0333ba54190607ea97f78303
Yulia Shvets <yuliia-mariia.shvets.ir.2021@lpnu.ua>
2022-05-04 15:00:19 +0300

    Added spotbugs and checkstyle to pom.xml

--- src/main/java/ua/lviv/iot/Main.java
+++ src/main/java/ua/lviv/iot/Main.java
@@ -1,13 +1,8 @@
 package ua.lviv.iot;
 
-import ua.lviv.iot.manager.ZooWriter;
-
-import java.io.IOException;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        ZooWriter.writeCSV();
     }
 }
