96d7227e5941691cda50d1bea792e9f3cd046e01
Yulia Shvets <yuliia-mariia.shvets.ir.2021@lpnu.ua>
2022-05-04 14:20:38 +0300

    Added ZooTest class

---
+++ src/test/java/ua/lviv/iot/manager/ZooTest.java
@@ -0,0 +1,38 @@
+package ua.lviv.iot.manager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ZooTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addBirds() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void foundBirds() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortByWeightAsc() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortByWeightDesc() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortByFeedAsc() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortByFeedDesc() {
+    }
+}
