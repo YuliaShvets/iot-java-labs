d90a337453bd5eb34368c38a0f885ea3595b169c
Yulia Shvets <yuliia-mariia.shvets.ir.2021@lpnu.ua>
2022-05-11 20:28:49 +0300

    Modified Main and class RegExpression added

--- src/main/java/ua/lviv/iot/Main.java
+++ src/main/java/ua/lviv/iot/Main.java
@@ -1,8 +1,21 @@
 package ua.lviv.iot;
 
+import ua.lviv.iot.manager.RegExpression;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        RegExpression regex = new RegExpression();
+        regex.replaceGrade("Ivanenko Ivan 67");
+        regex.replaceGrade("Petrenko Petro 23");
+        regex.replaceGrade("Vasylenko Vasyl 45");
+        regex.replaceGrade("Melnyk Andriy 75");
+        regex.replaceGrade("Kravchuk Serhii 99");
+
+
+
+
 
     }
 }
